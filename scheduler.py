477ebf82db04830bb5b1baebb01c48b044b56b9a
Damjan <damjandimitrov1@gmail.com>
2020-05-15 18:53:37 +0200

    Added comments

--- scheduler.py
+++ scheduler.py
@@ -3,20 +3,22 @@
 import os
 import schedule
 
+# Execute a system command to display a toast notification with a custom message
 def notify(title, text):
     os.system("""
               osascript -e 'display notification "{}" with title "{}"'
               """.format(text, title))
 
+# Delete all empty folders in a given directory
 def delete_empty_folders():
     notify('Script', 'Script is currently running')
     path = '/enter/path/here'
-    os.chdir(path)
-    directory = os.walk(path)
+    os.chdir(path)  # Change working directory to the path
+    directory = os.walk(path)  # Scan directory for its contents
     content = next(directory)
-    for dir in content[1]:
-        if not os.listdir(dir):
-            os.rmdir(dir)
+    for folder in content[1]:  # Loop through each folder in the path
+        if not os.listdir(folder):  # If it is empty remove it
+            os.rmdir(folder)
 
 schedule.every().day.do(delete_empty_folders)
 while True:
