099c984b10d63db5cbfbfd288c0fed1e6ef2d89b
Damjan <damjandimitrov1@gmail.com>
2020-05-15 18:47:15 +0200

    Implemented custom function as a job
    The function deletes all empty folders in a given directory

--- scheduler.py
+++ scheduler.py
@@ -2,7 +2,18 @@
 
 import os
 
+
 def notify(title, text):
     os.system("""
               osascript -e 'display notification "{}" with title "{}"'
               """.format(text, title))
+
+def delete_empty_folders():
+    notify('Script', 'Script is currently running')
+    path = '/enter/path/here'
+    os.chdir(path)
+    directory = os.walk(path)
+    content = next(directory)
+    for dir in content[1]:
+        if not os.listdir(dir):
+            os.rmdir(dir)
