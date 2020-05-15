f5dd87d4aa74ceaff1804aa4e227f554f6a6ccb0
Damjan <damjandimitrov1@gmail.com>
2020-05-15 18:37:34 +0200

    Implemented notification function

---
+++ scheduler.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import os
+
+def notify(title, text):
+    os.system("""
+              osascript -e 'display notification "{}" with title "{}"'
+              """.format(text, title))
